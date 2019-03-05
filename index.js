8733402dbbe13370ed6f92eee03aab1b7edc0358
nicksapp <changjie22@qq.com>
2019-03-05 13:45:33 +0800

    版本更新提示

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
 
 const path = require('path')
 const Command = require('common-bin');
-// const chalk = require('chalk');
-// const figlet = require('figlet');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const Store = require('data-store');
+const pkg = require('./package.json');
 
-// console.log(
-//   chalk.red(
-//     figlet.textSync('Ever', {
-//       horizontalLayout: 'fitted'
-//     })
-//   )
-// );
+console.log(
+  chalk.red(
+    figlet.textSync('WBHelper', {
+      horizontalLayout: 'fitted'
+    })
+  )
+);
 
 class MainCommand extends Command {
   constructor(rawArgv) {
@@ -20,6 +22,27 @@ class MainCommand extends Command {
 
     // register the entire directory to commands 注册所有继承 Command 指令
     this.load(path.join(__dirname, './lib/command'));
+
+    let qiyuCache = new Store({
+      path: `${__dirname}/data/cacheVersion.json`
+    });
+
+    const versionData = qiyuCache.get(pkg.name);
+    if (versionData) {
+      if (Date.now() - versionData.lastCheck > 1000 * 3600 * 24) {
+        try {
+          this.helper.spawn(
+            process.execPath,
+            [require('path').join(__dirname, './lib/checkUpdate.js'), JSON.stringify({
+              'name': pkg.name, // package 信息
+              'version': pkg.version,
+              'registry': 'https://registry.npmjs.org',
+              'updateMessage': 'Package update available: <%=colors.dim(current)%> -> <%=colors.green(latest)%> \nRun <%=colors.cyan(command)%> to update',
+              'level': 'minor' // 自定义强制更新的版本更新级别，默认是 major
+            })]);
+        } catch (e) {}
+      }
+    }
   }
 }
 
