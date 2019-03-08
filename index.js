06ddf49f08dd42b4e20eb84efbaac62a321a498e
nicksapp <changjie22@qq.com>
2019-03-08 14:59:34 +0800

    版本检查&nei prefix 支持&文件目录检查优化

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const chalk = require('chalk');
 const figlet = require('figlet');
 const Store = require('data-store');
 const pkg = require('./package.json');
+const { mkdirs } = require('./lib/file-generator');
 
 console.log(
   chalk.red(
@@ -22,27 +23,40 @@ class MainCommand extends Command {
 
     // register the entire directory to commands 注册所有继承 Command 指令
     this.load(path.join(__dirname, './lib/command'));
+    this.versionCheck();
+    
+  }
 
-    let qiyuCache = new Store({
-      path: `${__dirname}/data/cacheVersion.json`
+  versionCheck() {
+    let qiyuCache;
+    mkdirs(path.join(__dirname, './data/temp/'), () => {
+      qiyuCache = new Store({
+        path: `${__dirname}/./data/temp/cacheVersion.json`
+      });
     });
 
     const versionData = qiyuCache.get(pkg.name);
     if (versionData) {
       if (Date.now() - versionData.lastCheck > 1000 * 3600 * 24) {
-        try {
-          this.helper.spawn(
-            process.execPath,
-            [require('path').join(__dirname, './lib/checkUpdate.js'), JSON.stringify({
-              'name': pkg.name, // package 信息
-              'version': pkg.version,
-              'registry': 'https://registry.npmjs.org',
-              'updateMessage': 'Package update available: <%=colors.dim(current)%> -> <%=colors.green(latest)%> \nRun <%=colors.cyan(command)%> to update',
-              'level': 'minor' // 自定义强制更新的版本更新级别，默认是 major
-            })]);
-        } catch (e) {}
+        this._checkUpdate();
       }
+    } else {
+      this._checkUpdate()
     }
+  }
+
+  _checkUpdate() {
+    try {
+      this.helper.spawn(
+        process.execPath,
+        [require('path').join(__dirname, './lib/checkUpdate.js'), JSON.stringify({
+          'name': pkg.name, // package 信息
+          'version': pkg.version,
+          'registry': 'https://registry.npmjs.org',
+          'updateMessage': 'Package update available: <%=colors.dim(current)%> -> <%=colors.green(latest)%> \nRun <%=colors.cyan(command)%> to update',
+          'level': 'minor' // 自定义强制更新的版本更新级别，默认是 major
+        })]);
+    } catch (e) {}
   }
 }
 
